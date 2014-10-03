ebf364faa247eb72bb1de69c93ed9130b7816622
Matt Vanderpol <matt@vanderpol.net>
2014-10-03 09:22:18 -0700

    Test for empty context

--- test/styles_test.js
+++ test/styles_test.js
@@ -5,18 +5,24 @@ var expect = chai.expect;
 var Handlebars = require('handlebars');
 
 describe('{{#styles styles}}{{/styles}}', function() {
+  var source = '{{#styles styles}}{{/styles}}';
+  var template = Handlebars.compile(source);
+
   before(function() {
     require('../index.js').register(Handlebars, {});
   });
 
   it ('handles simple styles', function() {
-    var source = '{{#styles styles}}{{/styles}}';
-    var template = Handlebars.compile(source);
     var context = {
       styles: {
         'background-image': 'http://example.org/image.jpg'
       }
     };
     expect(template(context)).to.equal(' style="background-image:url(http://example.org/image.jpg);"');
+  });
+
+  it ('handles empty context', function() {
+    var context = {};
+    expect(template(context)).to.equal('');
   });
 });
